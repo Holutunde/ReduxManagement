755aa57b390bb5ea3c69495f39b1dffdd02a9d3c
Holutunde <Holutunde99@gmail.com>
2022-03-11 23:55:49 +0100

    added component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react'
-import Header from './containers/Header'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './containers/Header';
 
 const App = () => {
   return (
     <div className="App">
+      <Router>
+        
+      </Router>
       <Header />
     </div>
   )
