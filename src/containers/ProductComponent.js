b0b4a430623a2de277abed75d1284a4d49318a94
Holutunde <Holutunde99@gmail.com>
2022-03-13 23:39:00 +0100

    Modified component

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const ProductComponent = () => {
+  // const products = useSelector((state) => state.allProduct.products)
+  // console.log(products)
+  // const { id, title } = products[0]
   return (
-    <div>
-      <h1>ProductComponent</h1>
+    <div className="four column wide">
+      <div className="ui link cards">
+        <div className="card">
+          <div className="image"></div>
+          <div className="content">
+            <div className="header" style={{ marginTop: '100' }}></div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
+
+import { useDispatch, useSelector } from 'react-redux'
+import ProductComponent from './ProductComponent'
+import { setProducts } from '../redux/actions/productAction'
 
 const ProductListing = () => {
+  const { products } = useSelector((state) => state.allProducts)
+  console.log(products[3])
+  const dispatch = useDispatch()
+  const fetchProducts = async () => {
+    const response = await axios
+      .get('https://fakestoreapi.com/products')
+      .catch((err) => {
+        console.log('Err: ', err)
+      })
+    dispatch(setProducts(response.data))
+    //This will go to the action and will return the object taken by the reducer
+    console.log(response)
+  }
+
+  useEffect(() => {
+    fetchProducts()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
-    <div>
-      <h1>ProductListing</h1>
+    <div className="ui grid container">
+      <ProductComponent />
     </div>
   )
 }
