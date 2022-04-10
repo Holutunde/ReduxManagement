9ae7cdc232a8d970c6ab374956988774c24d9f60
Holutunde <Holutunde99@gmail.com>
2022-04-10 23:46:26 +0100

    Layout Adjustment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './containers/Header'
 import ProductDetail from './containers/ProductDetail'
